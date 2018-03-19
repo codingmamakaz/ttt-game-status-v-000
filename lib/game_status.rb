1cad79cd970b70d6c4a5e66dede2a999cf80b1ad
Kazumi Karbowski <kazkazinus@gmail.com>
2018-03-19 03:00:57 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,27 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0, 1, 2], #Top row
+  [3, 4, 5], #Middle row
+  [6, 7, 8], #Bottom row
+  [0, 3, 6], #Left column
+  [1, 4, 7], #Middle column
+  [2, 5, 8], #Right column
+  [0, 4, 8], #left diagonal
+  [2, 4, 6]  #Right diagnal
+]
+
+
+def won?(board)
+  WIN_COMBINATIONS.each do |win_combination|
+    win_combination.each do |index|
+
+      if (board[win_combination[0]] == "X" && board[win_combination[1]] == "X" && board[win_combination[2]] == "X") || (board[win_combination[0]] == "O" && board[win_combination[1]] == "O" && board[win_combination[2]] == "O")
+    return win_combination
+      
+
+      end
+    end
+  end
+end
