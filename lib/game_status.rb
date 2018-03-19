e7e9d093eda35774d2cbeb87c1e16d63b3f979c1
Kazumi Karbowski <kazkazinus@gmail.com>
2018-03-19 03:06:08 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -22,7 +22,8 @@ def won?(board)
 
       if (board[win_combination[0]] == "X" && board[win_combination[1]] == "X" && board[win_combination[2]] == "X") || (board[win_combination[0]] == "O" && board[win_combination[1]] == "O" && board[win_combination[2]] == "O")
     return win_combination
-      
+  else
+    false
 
       end
     end
